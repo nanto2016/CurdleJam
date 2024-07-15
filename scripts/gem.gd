aec7cc8f88bb65400bf0243c541d926fd16b6dae
nanto2016 <vadebonant@gmail.com>
2024-07-14 22:37:05 -0400

    Gems update #1
    Gems are now in the game! The more you carry, the heavier you are. Left-Click (or Q) to throw them and Right-Click (or E) to pick them up.
    What doesn't work: indicator showing number of gems doesn't work, I'll fix that, and maybe other stuff too I'll continue working on it

--- scripts/gem.gd
+++ scripts/gem.gd
@@ -1,19 +1,14 @@
-extends CharacterBody2D
-
-@export var gravity: float = 980
-var angular_velocity: float
-
+extends RigidBody2D
 
 func _ready():
 	$Sprite.play(StringName("gem" + var_to_str(randi_range(0, 5))))
+	gravity_scale = 0.2
 
 
 func throw(force: Vector2):
-	velocity = force
-	angular_velocity = randf_range(-0.5, 0.5)
+	apply_central_impulse(force)
+	apply_torque_impulse(randf_range(-0.01, 0.01))
 
 
-func _physics_process(delta):
-	rotate(angular_velocity)
-	velocity.y += gravity * delta
-	move_and_slide()
+func _on_freeze_body_entered(body):
+	freeze = true
--- scripts/player.gd
+++ scripts/player.gd
@@ -15,14 +15,16 @@ extends CharacterBody2D
 @export_range(100, 1000, 50, "suffix:px/s²") var gravity: int
 # Air acceleration
 @export_range(10.0, 100.0, 10.0, "suffix:px/s²") var air_acceleration: float
-# Air deceleration
+## Added to velocity
 @export_range(10.0, 100.0, 10.0, "suffix:px/s²") var air_deceleration: float
-# Jump height
+## Exact vertical displacement of the player when jumping, in pixels. This actually works: there are calculation made when jumping based on gravity, so whatever the gravity (even if it changes in-game) the player will jump this high
 @export_range(10, 100, 5, "suffix:px") var jump_height: int
-# Wall jump
+## Vector added to velocity when wall jumping
 @export var wall_jump_direction: Vector2
-# Wall slide
+## Max speed at which you can go while sliding on a wall
 @export_range(10.0, 100.0, 5, "suffix:px/s") var wall_slide_speed: float
+## How much acceleration towards the ground each Gem will it add to gravity
+@export_range(1.0, 10.0, 0.5, "suffix:px/s²") var gem_weight: float
 
 @onready var sprite: AnimatedSprite2D = $Sprite
 @onready var left: RayCast2D = $Left
@@ -37,9 +39,26 @@ var running: bool = false
 var wall_sliding: bool = false
 var hanging: bool = false
 var ledge_grabbing: bool = false
+var show_range: bool = false:
+	set(value):
+		show_range = value
+		if value == true:
+			$"Pickup range".start()
+		queue_redraw()
 
 var looking_direction: int = -1
-var gems: int
+var gems: int:
+	set(value):
+		gems = value
+		$Sprite/Gems.size.y = 8 * ((value + 1) % 3 + 1)
+		$Sprite/Gems.size.x = 8 * int((value + 1) / 3.0 + 1)
+		print("YOU NOW HAVE ", value, " GEMS")
+
+
+func _ready():
+	gems = 6
+	push_warning("CAUTION: PLAYER START WITH ", gems, " GEMS")
+
 
 func _physics_process(delta):
 	if not ledge_grabbing:
@@ -48,10 +67,15 @@ func _physics_process(delta):
 		
 		if Input.is_action_just_pressed("throw"):
 			if gems > 0:
-				var new_gem: CharacterBody2D = gem.instantiate()
-				add_child(new_gem)
-				new_gem.throw(Vector2(100, 0))
-				gems -= 1
+				throw_gem()
+		
+		if Input.is_action_just_pressed("pickup"):
+			show_range = true
+			var nearby_gems: Array = $Pickup.get_overlapping_bodies()
+			if nearby_gems.size() > 0:
+				# Pick up a gem
+				nearby_gems[0].queue_free()
+				gems += 1
 		
 		if is_on_floor(): # On Floor
 			if in_air:
@@ -105,7 +129,6 @@ func _physics_process(delta):
 					left.force_raycast_update()
 					right.force_raycast_update()
 					if just_jumped and ((direction > 0 and not left.is_colliding()) or (direction < 0 and not right.is_colliding())):
-						print(int(direction))
 						ledge_grab(int(direction))
 						ledge_grabbing = true
 						direction = 0
@@ -187,7 +210,7 @@ func _physics_process(delta):
 			if not direction:
 				velocity.x = move_toward(velocity.x, 0, air_deceleration)
 			
-			velocity = Vector2(velocity.x, clamp(velocity.y + gravity * delta, -INF, wall_slide_speed if wall_sliding else INF))
+			velocity = Vector2(velocity.x, clamp(velocity.y + gravity * delta + gem_weight * gems, -INF, wall_slide_speed if wall_sliding else INF))
 			
 			if velocity.y > 0 and not wall_sliding and not sprite.animation == StringName("fall"):
 				sprite.play("fall")
@@ -228,3 +251,20 @@ func _on_ledge_grab_animation_finished(_anim_name):
 	global_position = sprite.global_position
 	sprite.position = Vector2.ZERO
 	ledge_grabbing = false
+
+
+func throw_gem():
+	var new_gem: RigidBody2D = gem.instantiate()
+	new_gem.global_position = global_position
+	get_parent().add_child(new_gem)
+	new_gem.throw(get_local_mouse_position().limit_length(150))
+	gems -= 1
+
+
+func _draw():
+	if show_range:
+		draw_arc(Vector2.ZERO, $Pickup/Shape.shape.radius, 0, TAU, 20, Color.WHITE)
+
+
+func _on_pickup_range_timeout():
+	show_range = false
